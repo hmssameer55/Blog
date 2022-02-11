f207ed546bbda31c7d898eac72c137a74acb52bb
Sameer <hmssameer55@gmail.com>
2022-02-11 16:25:03 +0530

    ha

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.post('/compose', async (req, res) => {
   
 })
 
-app.listen(3000,  ()=> {
+app.listen(process.env.PORT || 3000,  ()=> {
   console.log("Server started on port 3000");
   makeDbConnection()
 });
